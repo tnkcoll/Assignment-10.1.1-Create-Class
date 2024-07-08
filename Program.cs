00b092e7ef89e0cb6449bb7bb1ba10f5840e6122
tnkcoll <theodore.collins2023@outlook.com>
2024-07-08 13:23:28 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace Assignment_10._1._1_Create_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var forecast = new Terminal_Aerodrome_Forecast
+            {
+                Date = 240708,
+                StartTime = 1100,
+                EndTime = 1900,
+                Group = "BECMG",
+                WindDirection = 180,
+                WindSpeed = 10,
+                Visibility = "7SM",
+                AltimeterSetting = 30.12,
+                Turbulence = 50103,
+                Icing = 60104,
+                PresentWeather = "NSW"
+            };
+
+            string jsonString = JsonSerializer.Serialize(forecast);
+
+            Console.WriteLine(jsonString);
+
+            XmlSerializer mySerializer = new XmlSerializer(typeof(Terminal_Aerodrome_Forecast));
+
+            var sw = new StreamWriter("D://MSSA//myFileName.xml");
+            mySerializer.Serialize(sw, forecast);
+            sw.Close();
+        }
+    }
+}
---
+++ Terminal Aerodrome Forecast.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_10._1._1_Create_Class
+{
+    public class Terminal_Aerodrome_Forecast
+    {
+
+        public int Date { get; set; }
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
+        public string? Group { get; set; }
+        public int WindDirection { get; set; }
+        public int WindSpeed { get; set; }
+        public string? Visibility { get; set; }
+        public double AltimeterSetting { get; set; }
+        public int Turbulence { get; set; }
+        public int Icing { get; set; }
+        
+        private string? presentWeather;
+
+        public string PresentWeather
+        {
+            get { return presentWeather; }
+            set { presentWeather = value; }
+            
+        }
+    }
+}
